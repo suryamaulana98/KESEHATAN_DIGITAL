a9002358ecc174d4cca2b7bc5851196cdcf4bbdc
ilhamvfx <133854062+muhammadilham1001@users.noreply.github.com>
2023-11-15 14:31:17 +0700

    controller

--- kesehatan_digital/app/Http/Controllers/DapodikController.php
+++ kesehatan_digital/app/Http/Controllers/DapodikController.php
@@ -4,14 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class userController extends Controller
+class DapodikController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        return view('admin.user');
+        //
     }
 
     /**
