2692549167e89db1d0f36cbbd6625aac6353c55a
ilhamvfx <133854062+muhammadilham1001@users.noreply.github.com>
2023-12-02 22:43:37 +0700

    database

--- kesehatan_digital/database/migrations/2023_12_02_153718_create_ttds_table.php
+++ kesehatan_digital/database/migrations/2023_12_02_153718_create_ttds_table.php
@@ -11,20 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('ttd', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->unsignedBigInteger('id_kelas');
             $table->foreign('id_kelas')->references('id')->on('kelas')->onUpdate('cascade')->onDelete('cascade');
-            $table->string('role')->default('user');
-            $table->string('nis')->nullable();
-            $table->string('tinggi_badan')->nullable();
-            $table->string('berat_badan')->nullable();
-            $table->string('d_vaksin')->nullable();
-            $table->rememberToken();
+            $table->enum('status',['selesai','belum']);
             $table->timestamps();
         });
     }
@@ -34,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('ttds');
     }
 };
