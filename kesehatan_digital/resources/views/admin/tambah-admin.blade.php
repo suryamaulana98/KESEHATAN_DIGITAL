f23797b05fb08c2e628ab9444c21a6daac8c5476
AdimasCH <120011147+AdimasCH@users.noreply.github.com>
2023-12-05 20:50:20 +0700

    admin
    tampilan admin

---
+++ kesehatan_digital/resources/views/admin/tambah-admin.blade.php
@@ -0,0 +1,327 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- Mirrored from eliteadmin.themedesigner.in/demos/bt4/inverse/form-basic.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 02 Nov 2023 01:00:54 GMT -->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicon.png">
+    <title>Elite Admin Template - The Ultimate Multipurpose admin template</title>
+    <!-- Custom CSS -->
+    <link href="{{ asset('template_admin/inverse/dist/css/style.min.css') }}" rel="stylesheet">
+    <!-- page css -->
+    <link href="{{ asset('template_admin/inverse/dist/css/pages/file-upload.css') }}" rel="stylesheet">
+    <!-- Add this to the head section -->
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+
+<body class="skin-default fixed-layout">
+    <!-- ============================================================== -->
+    <!-- Preloader - style you can find in spinners.css -->
+    <!-- ============================================================== -->
+
+    <!-- ============================================================== -->
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <div id="main-wrapper">
+        @include('partials.topbar_admin')
+        @extends('partials.sidebar_admin')
+        <!-- ============================================================== -->
+        <div class="page-wrapper">
+            <!-- ============================================================== -->
+            <!-- Container fluid  -->
+            <!-- ============================================================== -->
+            <div class="container-fluid">
+                <!-- ============================================================== -->
+                <!-- Bread crumb and right sidebar toggle -->
+                <!-- ============================================================== -->
+                <div class="row page-titles">
+                    <div class="col-md-5 align-self-center">
+                        <h4 class="text-themecolor">Admin</h4>
+                    </div>
+                    <div class="col-md-7 align-self-center text-end">
+                        <div class="d-flex justify-content-end align-items-center">
+                            <ol class="breadcrumb justify-content-end">
+                                <li class="breadcrumb-item">
+                                    <a href="javascript:void(0)">Home</a>
+                                </li>
+                                <li class="breadcrumb-item active">Tambah Admin</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+                <!-- ============================================================== -->
+                <!-- End Bread crumb and right sidebar toggle -->
+                <!-- ============================================================== -->
+                <!-- ============================================================== -->
+                <!-- Start Page Content -->
+                <!-- ============================================================== -->
+                <!-- row -->
+
+                <!-- row -->
+                <!-- row -->
+
+                <!-- row -->
+                <!-- .row -->
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="card card-body">
+                            <h4 class="card-title">Tambah Admin</h4>
+                            <form action="{{ route('artikelAdmin.store') }}" method="POST"
+                                enctype="multipart/form-data" class="form-horizontal mt-4">
+                                @csrf
+                                <div class="form-group">
+                                    <label class="form-label">Nama</label>
+                                    <input type="text" name="nama" class="form-control"
+                                        placeholder="Masukkan Nama">
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="form-label">NIS</label>
+                                    <input type="text" name="nis" class="form-control"
+                                        placeholder="Masukkan NIS">
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="form-label">Email</label>
+                                    <input type="email" name="email" class="form-control"
+                                        placeholder="Masukkan Email">
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="form-label">Password</label>
+                                    <input type="password" name="password" class="form-control"
+                                        placeholder="Masukkan Password">
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="form-label">Role</label>
+                                    <select class="form-select col-12" name="role" id="inlineFormCustomSelect">
+                                        <option selected>--Pilih--</option>
+                                        @foreach ($data as $item)
+                                            
+                                        <option value="{{ $item->id }}">{{ $item->role }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+
+                                <button class="btn btn-success" style="float: right;">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.row -->
+                <!-- row -->
+
+                <!-- Row -->
+                <!-- ============================================================== -->
+                <!-- End Page Content -->
+                <!-- ============================================================== -->
+                <!-- ============================================================== -->
+                <!-- Right sidebar -->
+                <!-- ============================================================== -->
+                <!-- .right-sidebar -->
+                <div class="right-sidebar">
+                    <div class="slimscrollright">
+                        <div class="rpanel-title"> Service Panel
+                            <span>
+                                <i class="ti-close right-side-toggle"></i>
+                            </span>
+                        </div>
+                        <div class="r-panel-body">
+                            <ul id="themecolors" class="m-t-20">
+                                <li>
+                                    <b>With Light sidebar</b>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)" data-skin="skin-default"
+                                        class="default-theme working">1</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)" data-skin="skin-green" class="green-theme">2</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)" data-skin="skin-red" class="red-theme">3</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)" data-skin="skin-blue" class="blue-theme">4</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)" data-skin="skin-purple" class="purple-theme">5</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)" data-skin="skin-megna" class="megna-theme">6</a>
+                                </li>
+                                <li class="d-block m-t-30">
+                                    <b>With Dark sidebar</b>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)" data-skin="skin-default-dark"
+                                        class="default-dark-theme ">7</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)" data-skin="skin-green-dark"
+                                        class="green-dark-theme">8</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)" data-skin="skin-red-dark"
+                                        class="red-dark-theme">9</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)" data-skin="skin-blue-dark"
+                                        class="blue-dark-theme">10</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)" data-skin="skin-purple-dark"
+                                        class="purple-dark-theme">11</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)" data-skin="skin-megna-dark"
+                                        class="megna-dark-theme ">12</a>
+                                </li>
+                            </ul>
+                            <ul class="m-t-20 chatonline">
+                                <li>
+                                    <b>Chat option</b>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)">
+                                        <img src="../assets/images/users/1.jpg" alt="user-img" class="img-circle">
+                                        <span>Varun Dhavan
+                                            <small class="text-success">online</small>
+                                        </span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)">
+                                        <img src="../assets/images/users/2.jpg" alt="user-img" class="img-circle">
+                                        <span>Genelia Deshmukh
+                                            <small class="text-warning">Away</small>
+                                        </span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)">
+                                        <img src="../assets/images/users/3.jpg" alt="user-img" class="img-circle">
+                                        <span>Ritesh Deshmukh
+                                            <small class="text-danger">Busy</small>
+                                        </span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)">
+                                        <img src="../assets/images/users/4.jpg" alt="user-img" class="img-circle">
+                                        <span>Arijit Sinh
+                                            <small class="text-muted">Offline</small>
+                                        </span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)">
+                                        <img src="../assets/images/users/5.jpg" alt="user-img" class="img-circle">
+                                        <span>Govinda Star
+                                            <small class="text-success">online</small>
+                                        </span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)">
+                                        <img src="../assets/images/users/6.jpg" alt="user-img" class="img-circle">
+                                        <span>John Abraham
+                                            <small class="text-success">online</small>
+                                        </span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)">
+                                        <img src="../assets/images/users/7.jpg" alt="user-img" class="img-circle">
+                                        <span>Hritik Roshan
+                                            <small class="text-success">online</small>
+                                        </span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)">
+                                        <img src="../assets/images/users/8.jpg" alt="user-img" class="img-circle">
+                                        <span>Pwandeep rajan
+                                            <small class="text-success">online</small>
+                                        </span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- ============================================================== -->
+                <!-- End Right sidebar -->
+                <!-- ============================================================== -->
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Container fluid  -->
+            <!-- ============================================================== -->
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Page wrapper  -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- footer -->
+        @include('partials.footer_admin')
+        <!-- ============================================================== -->
+        <!-- End footer -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- End Wrapper -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.ckeditor.com/4.13.1/standard/ckeditor.js"></script>
+    <script>
+        CKEDITOR.replace('content');
+    </script>
+    <script src="{{ asset('template_admin/assets/node_modules/jquery/dist/jquery.min.js') }}"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="{{ asset('template_admin/assets/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="{{ asset('template_admin/inverse/dist/js/perfect-scrollbar.jquery.min.js') }}"></script>
+    <!--Wave Effects -->
+    <script src="{{ asset('template_admin/inverse/dist/js/waves.js') }}"></script>
+    <!--Menu sidebar -->
+    <script src="{{ asset('template_admin/inverse/dist/js/sidebarmenu.js') }}"></script>
+    <!--Custom JavaScript -->
+    <script src="{{ asset('template_admin/inverse/dist/js/custom.min.js') }}"></script>
+    <!-- ============================================================== -->
+    <!-- This page plugins -->
+    <!-- ============================================================== -->
+    <!--morris JavaScript -->
+    <script src="{{ asset('template_admin/assets/node_modules/raphael/raphael-min.js') }}"></script>
+    <script src="{{ asset('template_admin/assets/node_modules/morrisjs/morris.min.js') }}"></script>
+    <script src="{{ asset('template_admin/assets/node_modules/jquery-sparkline/jquery.sparkline.min.js') }}"></script>
+    <!-- Popup message jquery -->
+    <script src="{{ asset('template_admin/assets/node_modules/toast-master/js/jquery.toast.js') }}"></script>
+    <!-- Chart JS -->
+    <script src="{{ asset('template_admin/inverse/dist/js/dashboard1.js') }}"></script>
+    <script src="{{ asset('template_admin/assets/node_modules/toast-master/js/jquery.toast.js') }}"></script>
+</body>
+
+
+<!-- Mirrored from eliteadmin.themedesigner.in/demos/bt4/inverse/form-basic.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 02 Nov 2023 01:00:55 GMT -->
+
+</html>
