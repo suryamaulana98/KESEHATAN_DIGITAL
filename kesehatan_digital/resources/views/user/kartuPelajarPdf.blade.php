680da24363b0b98f09fdb88364025c36e203f2b1
ilhamvfx <133854062+muhammadilham1001@users.noreply.github.com>
2024-02-07 09:31:34 +0700

    kartu pelajar cetak card

--- kesehatan_digital/resources/views/user/kartuPelajarPdf.blade.php
+++ kesehatan_digital/resources/views/user/kartuPelajarPdf.blade.php
@@ -70,13 +70,18 @@
                 <div class="modal-header bg-primary text-white">
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body"> <div >
+                <div class="modal-body">
+                    <div>
+                        <?php
+                        $userPhotoPath = public_path('foto/' . Auth::user()->foto);
+                        $userPhotoData = file_get_contents($userPhotoPath);
+                        $userPhotoBase64 = base64_encode($userPhotoData);
+                        ?>
                         <img class="cardd" src="data:image/svg+xml;base64,<?php echo base64_encode(file_get_contents(public_path('img/background1.png'))); ?>" width="120">
                         <div class="card-body">
 
-                            {{-- <img src="{{ asset('img/' . Auth::user()->foto) }}"
-                                style="border-radius: 80%; width:25%;height:25%; margin-bottom:-65%; margin-left: 5%;"
-                                alt=""> --}}
+                            <img src="data:image/jpg;base64,{{ $userPhotoBase64 }}"
+                                style="border-radius: 100%; width:20%; height:15%; z-index: 2;" alt="">
                             <p class="card-text text-uppercase" style="margin-left:340px; margin-top: -215px;">
                                 {{ Auth::user()->name }}</p>
                             <p class="card-text text-uppercase" style="margin-left:340px; margin-top: 5px;">
@@ -86,7 +91,7 @@
                             <p class="card-text text-uppercase" style="margin-left:340px; margin-top: 4px;">
                                 {{ Auth::user()->kelas->kelas }}</p>
                             <p class="card-text text-uppercase" style="margin-left:340px; margin-top: 4px;">
-                                {{ Auth::user()->tanggal_lahir }}   
+                                {{ Auth::user()->tanggal_lahir }}
                             </p>
                             <p class="card-text text-uppercase" style="margin-left:340px; margin-top: 4px;">
                                 {{ Auth::user()->jenis_kelamin }}</p>
